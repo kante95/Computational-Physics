dac525bcad12184b45b2190873cf880a873ce007
Marco Canteri <canterimarco@gmail.com>
2017-03-21 18:55:42 +0100

    verlet

--- simul/simul.py
+++ simul/simul.py
@@ -21,6 +21,18 @@ class particle:
 		self.z = z
 	def __str__(self):
 		return "("+str(self.x)+","+str(self.y)+","+str(self.z)+")"
+	def __add__(self, other):
+		return particle(self.x+other.x,self.y+other.y,self.z+other.z)
+	def __sub__(self, other):
+		return particle(self.x-other.x,self.y-other.y,self.z-other.z)
+	def __mul__(self, other):
+		return particle(self.x*other,self.y*other,self.z*other)
+	def __truediv__(self, other):
+		return particle(self.x/other,self.y/other,self.z/other)
+
+
+def particle_rint(p):
+	return particle(np.round(p.x),np.round(p.y),np.round(p.z))
 
 def initialize(N,L):
 	i = np.floor(nthroot(N,3))
@@ -31,17 +43,27 @@ def initialize(N,L):
 		pr.append(particle(np.random.uniform(-L/2, L/2),np.random.uniform(-L/2, L/2),np.random.uniform(-L/2, L/2)))
 	return pr
 
-def simulation(num_particle, density, time, step):
-	particle = initialize(num_particle)
+def potential(particle,particle_istance):
+	#shit, this will be challenging
 
+
+def simulation(num_particle,L, density, time, step):
+	particle = initialize(num_particle, L)
+	
 	#verlet algorithm
+	mesh = np.arange(0,time,step)
+	supermegaipermatrix = [initialize(num_particle,L) for i in mesh] #oh yeah mothafucka!
+	np.insert(supermegaipermatrix,[0],particle)
+
+	for t in range(len(mesh)-1):
+		for j in range(len(particle)):
+			new_coordinate = 2*supermegaipermatrix[j][t]-supermegaipermatrix[j][t-1]-(1/m)*potential(j,supermegaipermatrix[:][t]) #will this create a new supermegaipermatrix in my memory? I hope it doesn't
+			supermegaipermatrix[j][t+1] = new_coordinata - L*particle_rint(new_coordinata/L) #boundary condition
+
 
 	#time avarage
 	#P = 
 
-	return P
+	#return P
 
-a = initialize(125,5)
-print len(a)
-for i in a:
-	print i
+simulation(125,5,1,10,1)
