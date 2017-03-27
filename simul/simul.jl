6a70ca492c87c25b283e8cc8c3d738ab84ee3be5
Marco Canteri <canterimarco@gmail.com>
2017-03-27 18:55:17 +0200

    added random velocities

--- simul/simul.jl
+++ simul/simul.jl
@@ -1,6 +1,10 @@
-function initialize(N,L)
+#Box-Muller formula
+normal_rand(std) = √(-2(std^2)*log(1-rand(Float64)))*cospi(2rand(Float64))
+
+function initialize(N,L,temperature)
     i = ∛N
     positions = zeros(N,3)
+    velocities = zeros(N,3)
     index = 1
     for j ∈ 0:L/(i-1):i+1
         for k ∈ 0:L/(i-1):i+1
@@ -8,6 +12,9 @@ function initialize(N,L)
                 positions[index,1]= j - L/2
                 positions[index,2] = k - L/2
                 positions[index,3] = m - L/2
+                velocities[index,1]= normal_rand(temperature)
+                velocities[index,2] = normal_rand(temperature)
+                velocities[index,3] = normal_rand(temperature)
                 index+=1
             end
         end
@@ -16,11 +23,11 @@ function initialize(N,L)
     #for j ∈ 0:N-i^3
     #    append!(position,rand(-L/2:L/2,3))
     #end
-    velocities = zeros(positions)
+
     return positions,velocities
 end
 
-lennard_jones(r) = 4(r^-12 - r^-6)
+lennard_jones(r) =  4(r^-12 - r^-6)
 
 function lennard_jones_derivative!(r,returning)
     den = r[1]^2+r[2]^2 +r[3]^2
@@ -29,13 +36,12 @@ function lennard_jones_derivative!(r,returning)
     returning[3] = -4(-14r[3]*den^-7 + 8r[3]*den^-4)
 end
 
-
 function calculate_accellerations_and_potential(positions,N,L)
     accelerations = zeros(positions)
     potentials = zeros(125)
     force = Array{Float64}(3)
     for i ∈ 1:N
-       for j ∈ i+1:N
+        for j ∈ i+1:N
             r = positions[i,:]-positions[j,:]-round((positions[i,:]-positions[j,:])/L)
             dist = √(r[1]^2+r[2]^2 +r[3]^2)
             if dist<L/2
@@ -50,11 +56,11 @@ function calculate_accellerations_and_potential(positions,N,L)
     return accelerations,potentials
 end
 
-function simulation(num_particles,L, density, temperature, time, step)
+function simulation(num_particles, density, temperature, time, step)
+    V = num_particles/density
+    L = ∛V
     print("Inizializzo lo stato iniziale....\n")
-    positions,velocities = initialize(num_particles, L)
-    #mesh = collect(step,time,step)
-    #energy = zeros(lenght(mesh))
+    positions,velocities = initialize(num_particles, L,temperature)
     #velocity verlet algorithm
     current_accelerations,current_potentials = calculate_accellerations_and_potential(positions,num_particles,L)
     for t ∈ step:step:time
@@ -75,11 +81,11 @@ function simulation(num_particles,L, density, temperature, time, step)
         print("Energy at second $t: $energy \n")
     end
         #average =  what da fuck have i to put here??
-    #plt.plot(mesh,energy)
-    #plt.show()
     #time average
     #P = 
     #return P
 end
 
-simulation(125,5,1,273,5,0.01)
+#for ρ ∈ [0.1,0.01,0.001]
+    simulation(125,0.01,273,5.0,0.01)
+#end
