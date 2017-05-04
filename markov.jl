4939736784c8f0c1e37bb5b3d79f6c0e546dab1f
kante95 <canterimarco@gmail.com>
2017-05-04 18:36:16 +0200

    Update markov.jl

--- markov.jl
+++ markov.jl
@@ -1,9 +1,9 @@
 random(from,to) = rand(Float64)*(to-from) + from
 
-println("Dimmi un delta:")
-delta = float(readline(STDIN))
+#println("Dimmi un delta:")
+#delta = float(readline(STDIN))
 acceptance = 0
-#delta = 0.5
+delta = 0.5
 N = 1000000
 x = zeros(N)
 x[1] = random(-7,7)
@@ -35,3 +35,21 @@ dxmedio = sqrt((dxmedio/N-xmedio^2)/N)
 dx2medio = sqrt((dx2medio/N-x2medio^2)/N)
 acceptance_ratio = acceptance/N
 println("Valore medio x: $xmedio +/- $dxmedio, valore medio x^2: $x2medio +/- $dxmedio acceptance ratio: $(acceptance_ratio*100)%")
+
+
+l = 150
+c = zeros(l)
+
+D = x2medio - xmedio^2
+
+for i in 1:l
+	j = 1
+	while j+i<N
+		c[i]+= 	x[j]*x[j+i]
+		j+=1
+	end
+	c[i]/=j-1	
+	c[i] = (c[i] - xmedio^2)/D
+end
+
+print(c)
